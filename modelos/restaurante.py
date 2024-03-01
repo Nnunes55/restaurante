4feb87622fbb6691a04b6a91b8e3071ed1913b9d
Nathan Nunes <nathannunessp587@gmail.com>
2024-03-01 16:27:58 -0300

    Mudanças

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,28 +1,32 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
+
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+    
 
     @property
     def ativo(self):
-        return 'verdadeiro' if self.ativo else 'false'
+        return 'verdadeiro' if self._ativo else 'false'
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
+    def alternar_estado(self):
+        self.ativo = not self._ativo
 
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
-
-Restaurante.listar_restaurantes()
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
